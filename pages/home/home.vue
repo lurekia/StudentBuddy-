1f3e0e6725cd9f2f14e1664aeba701ecf80f60a0
lurekia <1393792692@qq.com>
2023-08-26 15:48:51 +0800

    加入uni-im

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,22 +1,18 @@
 <template>
 	<view>
-		home
+		<!-- <web-view src="http://127.0.0.1:5173/"></web-view> -->
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+<script setup>
+	
 </script>
 
 <style>
-
+#map {
+  height: 80vw;
+  width: 80vw;
+  margin: 100px 10vw;
+  background-color: purple;
+}
 </style>
